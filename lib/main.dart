93982a04a69bf16e85a6620787022e65509459f0
Sanikhsan12 <lordkazumasan12@gmail.com>
2025-11-11 01:03:24 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Placeholder(),
+      home: SplashScreen(),
     );
   }
 }
