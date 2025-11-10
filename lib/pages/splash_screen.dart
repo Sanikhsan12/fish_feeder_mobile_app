11183c2efe5d1497d951fcac69099d2d2d8d6d27
Sanikhsan12 <lordkazumasan12@gmail.com>
2025-11-11 03:47:42 +0700

    update pages

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import './auth/login_page.dart';
+import 'login_page.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class SplashScreen extends StatefulWidget {
