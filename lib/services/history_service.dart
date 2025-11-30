54a0f1243ef51d95e119ae312405d5c6202ba7e7
Sanikhsan12 <lordkazumasan12@gmail.com>
2025-11-30 18:07:30 +0700

    implementing pagination to service

--- lib/services/history_service.dart
+++ lib/services/history_service.dart
@@ -7,13 +7,36 @@ class HistoryService {
   final String baseUrl = dotenv.env['API_BASE_URL'] ?? '';
 
   // ! Get History data
-  Future<List<HistoryModel>> getHistoryData(
-      {int limit = 100, int page = 1}) async {
-    final offset = (page - 1) * limit;
-    final response = await http
-        .get(Uri.parse('$baseUrl/history?limit=$limit&offset=$offset'));
+  Future<Map<String, dynamic>> getHistoryData({
+    int page = 1,
+    int pageSize = 10,
+    String? month,
+    String? year,
+  }) async {
+    var url = '$baseUrl/history?page=$page&page_size=$pageSize';
+    if (month != null) url += '&month=$month';
+    if (year != null) url += '&year=$year';
+
+    final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      List<dynamic> data = json.decode(response.body);
+      final jsonBody = json.decode(response.body);
+      List<dynamic> data = jsonBody['data'];
+      final pagination = jsonBody['pagination'];
+      return {
+        'data': data.map((item) => HistoryModel.fromJson(item)).toList(),
+        'pagination': pagination,
+      };
+    } else {
+      throw Exception('Failed to load history data');
+    }
+  }
+
+  Future<List<HistoryModel>> getAllHistoryData() async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/history?page=1&page_size=2000'));
+    if (response.statusCode == 200) {
+      final jsonBody = json.decode(response.body);
+      List<dynamic> data = jsonBody['data'];
       return data.map((item) => HistoryModel.fromJson(item)).toList();
     } else {
       throw Exception('Failed to load history data');
